8f30b9451405f468cc703df5c84d014b7c99f5a6
Muhammad Nur Muiz <muhammadnurmuiz172@gmail.com>
2023-11-22 15:10:45 +0700

    Halaman Pembayaran & Pilih sebagai

---
+++ app/src/main/java/com/collaboracrew/catwell/view/Pilih_Sebagai.kt
@@ -0,0 +1,12 @@
+package com.collaboracrew.catwell.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.collaboracrew.catwell.R
+
+class Pilih_Sebagai : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pilih_sebagai)
+    }
+}
