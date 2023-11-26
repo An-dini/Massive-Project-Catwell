d0adcdcffa557c6d80450497deb0d79b69be8c35
unknown <dariel200356@gmail.com>
2023-11-26 12:53:14 +0800

    update layout detail veneritarian

--- app/src/main/java/com/collaboracrew/catwell/view/PembayaranChat.kt
+++ app/src/main/java/com/collaboracrew/catwell/view/PembayaranChat.kt
@@ -2,11 +2,18 @@ package com.collaboracrew.catwell.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.collaboracrew.catwell.R
 
 class PembayaranChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pembayaran_chat)
+
+        val backButton = findViewById(R.id.ivBack) as ImageView
+
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
