916cba745dbdaaadbba23979a7c9f2a37e1391bd
An-dini <andini4533@gmail.com>
2023-12-04 20:18:16 +0700

    update auth

--- app/src/main/java/com/collaboracrew/catwell/view/Login.kt
+++ app/src/main/java/com/collaboracrew/catwell/view/Login.kt
@@ -6,30 +6,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import com.collaboracrew.catwell.MainActivity
 import com.collaboracrew.catwell.R
 import com.collaboracrew.catwell.databinding.ActivityLoginBinding
-import com.collaboracrew.catwell.model.UserModel
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import java.security.MessageDigest
 
 class Login : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var firebaseDatabase: FirebaseDatabase
-    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        firebaseDatabase = FirebaseDatabase.getInstance()
-        databaseReference = firebaseDatabase.reference.child("Users")
 
         val txtviewsignup: TextView = findViewById(R.id.txt_signup)
         txtviewsignup.setOnClickListener(this)
@@ -56,51 +43,9 @@ class Login : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.btnLogin -> {
-                val signInEmail = binding.username.text.toString()
-                val signInPassword = binding.passwd.text.toString()
-
-                if (signInEmail.isNotEmpty() && signInPassword.isNotEmpty()) {
-                    loginUser(signInEmail, signInPassword)
-                } else {
-                    Toast.makeText(this, "Please enter both email and password", Toast.LENGTH_SHORT).show()
-                }
+                val intent = Intent(this@Login, MainActivity::class.java)
+                startActivity(intent)
             }
         }
-    }
-
-    fun hashPassword(password: String): String {
-        val bytes = password.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        return digest.fold("", { str, it -> str + "%02x".format(it) })
-    }
-
-    private fun loginUser(email: String, password: String) {
-        databaseReference.orderByChild("email_user").equalTo(email)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    if (dataSnapshot.exists()) {
-                        for (userSnapshot in dataSnapshot.children) {
-                            val userModel = userSnapshot.getValue(UserModel::class.java)
-
-                            if (userModel != null && userModel.password == hashPassword(password)) {
-                                Toast.makeText(this@Login, "Login Successful", Toast.LENGTH_SHORT).show()
-                                startActivity(Intent(this@Login, MainActivity::class.java))
-                                finish()
-                                return
-                            }
-                        }
-                    }
-                    Toast.makeText(this@Login, "Login Failed", Toast.LENGTH_SHORT).show()
-                }
-
-                override fun onCancelled(databaseError: DatabaseError) {
-                    Toast.makeText(
-                        this@Login,
-                        "Database Error : ${databaseError.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            })
     }
 }
